53e554577afec4525b3aad34828bd006a983119b
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-01-11 13:33:18 -0300

    refactor: function check import js file.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const javascript = document.getElementById("javascript");
 
-window.onload = checkImport(javascript);
+window.addEventListener("load", () => {
+  checkImport(javascript);
+});
 
-async function checkImport (script)  {
-    const jsFormat = ".js";
-      script.src.includes(jsFormat) ? alert('Hello, Master! :D') : alert('Import a script js to check the examples in "console.log"');
-    }
-
+function checkImport(script) {
+  const jsFormat = ".js";
+  script.src.endsWith(jsFormat)
+    ? alert("Hello, Master! :D")
+    : alert("Import a script js to check the examples in console.");
+}
